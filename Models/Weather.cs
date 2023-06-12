80557f79553f3159b8ab7c5b386ea9b4059912bf
RobyRAX <robyalexander571@gmail.com>
2023-06-12 19:27:05 +0700

    first commit

---
+++ Models/Weather.cs
@@ -0,0 +1,10 @@
+namespace WeatherAPI.Models;
+
+public class Weather
+{
+    public string? WeatherCode {get; set;}
+    public string? Temperature {get; set;}
+    public string? Humidity {get; set;}
+    public string? WindSpeed {get; set;}
+    public string? WindDir {get; set;} 
+}
